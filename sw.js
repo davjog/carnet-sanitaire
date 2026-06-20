b01f911596c9971d32b3858d6a3873716f1b09b8
davjog <davjoguet@free.fr>
2026-06-20 18:41:55 +0200

    Fix page blanche : épingler @babel/standalone@7 et corriger le service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'carnet-sanitaire-v2';
+const CACHE = 'carnet-sanitaire-v3';
 const FILES = ['./', './index.html'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
